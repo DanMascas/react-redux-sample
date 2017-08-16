6c88295ed36f03a2fbeb3aacbda3a0fbd53c7f7d
danM <danmascas@yahoo.com>
2017-08-16 23:16:49 +0300

    customizat header

--- dev/components/App.js
+++ dev/components/App.js
@@ -1,12 +1,22 @@
 import React, {PropTypes} from 'react';
 import Header from './common/Header';
+// import '../stiluri/index.css';
+
 
 export class App extends React.Component {
     render() {
+        var style = {
+              marginTop: 30
+        };
         return (
-            <div className="container-fluid">
-                <Header />
-                {this.props.children}
+            // <div className="container-fluid">
+            <div>
+                <div className='row'>
+                    <Header location={this.props}/>
+                </div>
+	            <div className='row' style={style}>
+	            	{this.props.children}
+	            </div>
             </div>
         );
     }
--- dev/components/common/Header.js
+++ dev/components/common/Header.js
@@ -1,17 +1,52 @@
 import React, {PropTypes} from 'react';
 import {Link, IndexLink} from 'react-router';
 
-const Header = () => {
-    return (
-        <nav>
-            <IndexLink to="/"
-                       activeClassName="active">Home</IndexLink>
-            {" | "}
-            <Link to="/filme" activeClassName="active">Filme</Link>
-             {" | "}
-            <Link to="/manage" activeClassName="active">Manage</Link>
-        </nav>
-    );
-};
+export default class Header extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      collapsed: true,
+    };
+  }
 
-export default Header;
+  toggleCollapse() {
+    const collapsed = !this.state.collapsed;
+    this.setState({collapsed});
+  }
+
+  render() {
+    const { location } = this.props;
+    const { collapsed } = this.state;
+    const navClass = collapsed ? "collapse" : "";
+
+    return (
+      <div className='row'>
+        <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle" onClick={this.toggleCollapse.bind(this)} >
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+            </div>
+            <div className={"navbar-collapse " + navClass} id="bs-example-navbar-collapse-1">
+              <ul className="nav navbar-nav">
+                <li>
+                  <IndexLink to="/" onClick={this.toggleCollapse.bind(this)} activeClassName="active">Home</IndexLink>
+                </li>
+                <li>
+                  <Link to="/filme" onClick={this.toggleCollapse.bind(this)} activeClassName="active">Filme</Link>
+                </li>
+                <li>
+                  <Link to="/manage" onClick={this.toggleCollapse.bind(this)} activeClassName="active">Manage</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
